e17d2b8490174d972b59d60ae987d58b2e77eb16
Tyler Collingridge <tylercollingridge@gmail.com>
2024-08-15 13:24:46 -0600

    feat: create itemObj
    - Create pseudocode structure for storing items and creating them on the
    ShoppingPage.

--- src/components/ShoppingPage/ShoppingPage.jsx
+++ src/components/ShoppingPage/ShoppingPage.jsx
@@ -1,5 +1,7 @@
 import NavBar from "../NavBar/NavBar";
 import classes from "./ShoppingPage.module.css";
+// import items (from AddItem?)
+// TODO: will items store the itemObj directly meaning that I do not have to call them here? (probably)
 
 const ShoppingPage = () => {
   return (
@@ -7,6 +9,9 @@ const ShoppingPage = () => {
       <NavBar />
       <section className={classes.body}>
         <h1>Shopping</h1>
+        <section className="items">
+          {/* for item in items (get items from AddItem component?) */}
+        </section>
       </section>
     </>
   );
